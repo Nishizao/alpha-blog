ccfbc7a5fc54080846c7a7ab654751c5b404e872
Nishizao <nishizao93@icloud.com>
2018-09-30 11:38:10 +0000

    create valid user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-    
+    validates :username, prescence: true,
+    uniqueness: {case_sensitive: false},
+    length: {minimum: 3, maximum: 25}
+    validates :password, prescence: true,
+    uniqueness: true,
+    length: {minimum: 3, maximum: 25}
 end
